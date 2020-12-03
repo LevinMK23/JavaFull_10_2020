92fe3f190e72e93c95bcdcafcd86aee2b52511c0
levinmk23 <levinmk23@gmail.com>
2020-12-03 22:01:52 +0300

    lesson7

---
+++ Java2/src/main/java/lesson6/homework/IoServer.java
@@ -0,0 +1,47 @@
+package lesson6.homework;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class IoServer {
+
+    private static ClientHandler handler;
+
+    public static void main(String[] args) throws IOException {
+        Thread readThread = new Thread(() -> {
+            try {
+                Scanner in = new Scanner(System.in);
+                while (in.hasNext()) {
+                    String msg = in.next();
+                    if (handler == null) {
+                        System.out.println("You have to wait connection");
+                        while (handler == null) {
+                            Thread.sleep(500);
+                        }
+                    }
+                    if (handler.isRunning()) {
+                        handler.sendMessage(msg);
+                        if (msg.equals("/quit")) {
+                            handler.setRunning(false);
+                            break;
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        readThread.setDaemon(true);
+        readThread.start();
+        try (ServerSocket server = new ServerSocket(8189)) {
+            while (true) {
+                Socket socket = server.accept();
+                handler = new ClientHandler(socket);
+                new Thread(handler).start();
+            }
+        }
+    }
+
+}
