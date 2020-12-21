0fca23ce90d9c0a85f5992391ef247f7c9d5b808
Михаил Левин <mikelevin@yandex-team.ru>
2020-12-21 21:43:42 +0300

    lesson3

---
+++ java3/src/main/java/lesson3/IoHistoryServiceImpl.java
@@ -0,0 +1,39 @@
+package lesson3;
+
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.List;
+
+public class IoHistoryServiceImpl implements HistoryService {
+
+    /**
+     * HomeWork
+     * */
+
+    @Override
+    public List<String> getHistory() {
+        return null;
+    }
+
+    @Override
+    public List<String> getHistory(int limit) {
+        // available + skip
+        // RandomAccessFile
+//        try {
+//            RandomAccessFile raf = new RandomAccessFile("1.txt", "rw");
+//            FileChannel channel = raf.getChannel();
+//            ByteBuffer buf = ByteBuffer.allocate(100);
+//
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+        return null;
+    }
+
+    @Override
+    public void saveHistory(List<String> history) {
+
+    }
+}
